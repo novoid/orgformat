0e40ea163286a19734ce95d2f17bbd4279dffaf7
Karl Voit <git@Karl-Voit.at>
2019-11-05 21:42:43 +0100

    run_unit_tests.sh: added update of HTML docu

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -7,4 +7,7 @@ PYTHONPATH=. pytest-3 orgformat/orgformat_test.py
 # mypy is checking the type annotations:
 PYTHONPATH=. mypy orgformat/orgformat.py
 
+# OK, this is not a unit test but this doesn't take long and updated docu is always good:
+./update_pydoc.sh
+
 #end
